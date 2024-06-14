7a8a312b0a5fa2c00f164828f8047872812282cc
MaralSI <maral.sagynalieva@gmail.com>
2024-06-14 12:30:44 +0600

    Update class1.py

--- class1.py
+++ class1.py
@@ -1,2 +1,10 @@
 from art import tprint
 tprint("PYTHON")
+
+class Hello:
+    def greet(self):
+        return "Hello, world!"
+
+class NewClass(Hello):
+    def greet_with_name(self, name):
+        return f"Hello, {name}!"

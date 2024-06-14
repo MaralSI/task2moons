4f3fc1edcff8741847cfb8632bd801049f8ff118
MaralSI <maral.sagynalieva@gmail.com>
2024-06-14 12:45:12 +0600

    Update class1.py

--- class1.py
+++ class1.py
@@ -8,3 +8,12 @@ class Hello:
 class NewClass(Hello):
     def greet_with_name(self, name):
         return f"Hello, {name}!"
+
+from class1 import NewClass
+
+def tptin(instance):
+    print(instance.greet_with_name("Alice"))
+
+if __name__ == "__main__":
+    instance = NewClass()
+    tptin(instance)
